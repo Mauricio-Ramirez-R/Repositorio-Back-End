407d6de1e9ba71b2a0112d89988b58e2f5a18de2
Mauricio Ramírez <20225526@esen.edu.sv>
2025-02-06 12:50:30 -0600

    .

--- app/Http/Controllers/EmailPreviewController.php
+++ app/Http/Controllers/EmailPreviewController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class EmailPreviewController extends Controller
 {
     public function __invoke() {
-        return "Ruta funcionando correctamente";
+        dd(request()->all()); // 👈 Esto imprimirá los datos y detendrá la ejecución
+
+        request()->validate([
+            'customer' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'payment_method' => ['required', 'in:1,2,3'],
+            'products' => ['required', 'array'],
+            'products.*.name' => ['required', 'string', 'max:50'],
+            'products.*.price' => ['required', 'numeric', 'gt:0'],
+            'products.*.quantity' => ['required', 'integer', 'gte:1'],
+        ]);
+
+        return view('EmailPreview', request()->all());
     }
 }
