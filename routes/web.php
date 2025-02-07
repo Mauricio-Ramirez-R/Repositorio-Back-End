44782a0b8fc16b90dfa6660e9e522548a89bb666
Mauricio Ramírez <20225526@esen.edu.sv>
2025-02-06 22:12:20 -0600

    .

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\EmailPreviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/email-preview', EmailPreviewController::class);
+Route::get('/email-preview', EmailPreviewController::class);
+
